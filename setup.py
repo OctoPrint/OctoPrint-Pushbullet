bff6ef9bec588d4739bf310d134e98c98dd4cdd3
Gina Häußge <osd@foosel.net>
2015-11-16 18:28:44 +0100

    Forgot the requirements in the new setup.py version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/OctoPrint/OctoPrint-Pushbullet"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["requests", "pushbullet.py"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
